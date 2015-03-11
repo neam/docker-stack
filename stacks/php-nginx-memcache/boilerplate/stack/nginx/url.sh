6f78d3444178571058102df4443f01d28c41dc8d
Fredrik Wollsén <fredrik@neam.se>
2015-03-11 22:56:58 +0100

    Smaller fixes

--- stacks/php-nginx-memcache/boilerplate/stack/nginx/url.sh
+++ stacks/php-nginx-memcache/boilerplate/stack/nginx/url.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
     export DOCKER_HOST_IP=$(echo $DOCKER_HOST | sed 's/tcp:\/\///' | sed 's/:[0-9.]*//')
-    export NGINX_PORT_80=$(docker-compose port nginx 80 | sed 's/[0-9.]*://')
-    export URL="http://$DOCKER_HOST_IP:$NGINX_PORT_80"
+    export WEB_PORT_80=$(docker-compose port web 80 | sed 's/[0-9.]*://')
+    export URL="http://$DOCKER_HOST_IP:$WEB_PORT_80"
 
     URL=$URL"$@"
 
