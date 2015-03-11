68f51059dc1dae545901e02d5bb479f077729281
Fredrik Wollsén <fredrik@neam.se>
2015-03-11 05:43:35 +0100

    Latest php config tweaks/tests

--- stacks/php-nginx-memcache/boilerplate/stack/php/run.sh
+++ stacks/php-nginx-memcache/boilerplate/stack/php/run.sh
@@ -11,12 +11,18 @@ for configfile in /app/stack/php/conf.d/*; do
 done
 
 # Setup config variables only available at runtime
-sed -i "s|\${FOO}|${FOO}|" /etc/php5/fpm/conf.d/app.ini
+sed -i "s|\${DISPLAY_PHP_ERRORS}|${DISPLAY_PHP_ERRORS}|" /etc/php5/fpm/conf.d/app.ini
 
 # Make all environment variables available to php-fpm at runtime
 echo "" > "/etc/php5/fpm/conf.d/env.ini"
 for var in $(env | cut -f1 -d=); do
   echo "env[$var] = \$${var}" >> "/etc/php5/fpm/conf.d/env.ini"
+done
+
+# TODO: Local only
+
+for configfile in /app/stack/php/conf.d-local/*; do
+    cp $configfile /etc/php5/fpm/conf.d/
 done
 
 # Work around permission errors locally TODO only if local
