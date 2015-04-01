dcd31ac36ded1f4e87745ef8577f183eb07505e1
Fredrik Wollsén <fredrik@neam.se>
2015-04-01 09:43:27 +0300

    Fixed local and remote permissions with debian-php-nginx stack

--- stacks/debian-php-nginx/stack/php/run.sh
+++ stacks/debian-php-nginx/stack/php/run.sh
@@ -17,14 +17,25 @@ cp /app/stack/php/php-fpm.conf /etc/php5/fpm/php-fpm.conf
 # Setup config variables only available at runtime
 sed -i "s|\${DISPLAY_PHP_ERRORS}|${DISPLAY_PHP_ERRORS}|" /etc/php5/fpm/conf.d/app.ini
 
-# TODO: Local only
+if [ "$RUNNING_LOCALLY" == "1" ]; then
 
 for configfile in /app/stack/php/conf.d-local/*; do
     cp $configfile /etc/php5/fpm/conf.d/
 done
 
-# Work around permission errors locally TODO only if local
-usermod -u 1000 www-data
+  # Work around permission errors locally by making sure that "www-data" uses the same uid and gid as the host volume
+  TARGET_UID=$(stat -c "%g" /app)
+  usermod -o -u $TARGET_UID www-data
+  TARGET_GID=$(stat -c "%g" /app)
+  groupmod -o -g $TARGET_GID www-data
+
+else
+
+  # Prepare writable directories - TODO: Prevent app from writing to /app
+  chmod -R 777 /app/foo/app/runtime
+  chmod -R 777 /app/foo/www/assets
+
+fi
 
 # Run the command sent as command line arguments
 php5-fpm
