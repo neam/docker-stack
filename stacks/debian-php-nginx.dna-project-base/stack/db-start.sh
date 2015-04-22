e99831a73a5d52327552e25f8c6abc28744e8e64
Fredrik Wollsén <fredrik@neam.se>
2015-04-22 23:30:00 +0200

    Helper scripts to start database container with persistent data outside the stack (to mimic a database as a service provider)

---
+++ stacks/debian-php-nginx.dna-project-base/stack/db-start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# fail on any error
+set -o errexit
+
+# always run from project root
+script_path=`dirname $0`
+cd $script_path/..
+
+# set config
+DBREF=adoveo
+
+# run actual commands
+cd ../docker-md-plugin/
+./commands mariadb:create $DBREF
+MD_DATABASE_PORT=$(cat .mariadb/port_$DBREF)
+MD_DATABASE_PASSWORD=$(cat .mariadb/pwd_$DBREF)
+cd -
+
+docker-compose run builder stack/db-set-local-config.sh $MD_DATABASE_PORT $MD_DATABASE_PASSWORD
+
+echo "Database ready and configured for local development"
+
+exit 0
