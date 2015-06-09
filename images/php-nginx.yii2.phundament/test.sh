39031fdc16769f3755f5daeff8a4f129b8d017da
Tobias Munk <schmunk@usrbin.de>
2015-06-09 02:38:51 +0200

    refactored php images - moved fpm to tpm-prod - added fpm-dev with lessc (for runtime asset compilation)

--- images/php-nginx.yii2.phundament/test.sh
+++ images/php-nginx.yii2.phundament/test.sh
@@ -5,3 +5,5 @@ set -e
 docker run --rm phundament/php ls -la /app-src
 docker run --rm phundament/php sh -c 'cd /app-src && cp .env-dist .env && ./yii'
 docker run --rm phundament/php sh -c 'cd /app-src && cp .env-dist .env && ./yii help app'
+
+docker run --rm phundament/php:5.6-fpm-dev sh -c 'lessc'
