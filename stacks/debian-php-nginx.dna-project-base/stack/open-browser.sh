0405d11c0969ccffd95f3a9af91266e97b3e2dae
Fredrik Wollsén <fredrik@neam.se>
2015-08-13 10:45:19 +0300

    Added some shorthand-scripts to interact with the stack

---
+++ stacks/debian-php-nginx.dna-project-base/stack/open-browser.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+open $(docker-stack local url "$1")
+
+exit 0
---
+++ stacks/debian-php-nginx.dna-project-base/stack/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# fail on any error
+set -o errexit
+
+# always run from project root
+script_path=`dirname $0`
+cd $script_path/..
+
+# run actual command
+docker-stack local up -d
+
+exit 0
