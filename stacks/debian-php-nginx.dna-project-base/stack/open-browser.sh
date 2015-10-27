0f6b7a1d9911e36819de8c310c7b020fcc5bc484
Fredrik Wollsén <fredrik@neam.se>
2015-10-27 17:28:33 +0100

    Various tweaks

--- stacks/debian-php-nginx.dna-project-base/stack/open-browser.sh
+++ stacks/debian-php-nginx.dna-project-base/stack/open-browser.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$DATA" == "" ]; then
+if [ "$DATA" == "" ] || [ "$DATA" == "%DATA%" ]; then
   echo "Using DATA=clean-db since no DATA env var was set. To set:"
   echo "  export DATA=example"
   echo "Then run this script again"
   DATA=clean-db
 fi
 
-open $(docker-stack local url router 80 $DATA._PROJECT_.local "$1")
+URL="$(docker-stack local url router 80 $DATA._PROJECT_.192.168.99.100.xip.io "$1")"
+
+echo "Opening URL:"
+echo $URL
+
+open $URL
 
 exit 0
