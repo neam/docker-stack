3cbb6228ffe10dbdf77a101001e0dd297ba3f737
Fredrik Wollsén <fredrik@neam.se>
2016-01-30 12:58:08 +0200

    Avoiding "ambigous redirect" due to unquoted paths in install and select scripts

--- cli/commands/select.sh
+++ cli/commands/select.sh
@@ -5,7 +5,7 @@ if [ "$DOCKERSTACK_DEBUG" == "1" ]; then
 fi
 
 BASEPATH="$DOCKER_STACK_SRC/stacks/$1"
-cat "$DOCKER_STACK_SRC/stacks/$1/.docker-stack" | sed "s|{BASEPATH}|$BASEPATH|" > $APP_HOME/.docker-stack
+cat "$DOCKER_STACK_SRC/stacks/$1/.docker-stack" | sed "s|{BASEPATH}|$BASEPATH|" > "$APP_HOME/.docker-stack"
 
 echo "Stack '$1' selected"
 
