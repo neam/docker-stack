5da4a586bc7abe582f2dd50779c70906291831e8
Fredrik Wollsén <fredrik@neam.se>
2016-05-19 14:46:55 +0300

    Neam dna project base stack start script makes sure no other container is using port 80

--- stacks/neam/debian-php-nginx.dna-project-base/stack/start.sh
+++ stacks/neam/debian-php-nginx.dna-project-base/stack/start.sh
@@ -7,6 +7,9 @@ set -o errexit
 script_path=`dirname $0`
 cd $script_path/..
 
+# make sure no other container is using port 80
+docker ps | grep '0.0.0.0:80->' | awk '{ print $1 }' | xargs docker kill
+
 # run actual command
 docker-stack local up -d
 
