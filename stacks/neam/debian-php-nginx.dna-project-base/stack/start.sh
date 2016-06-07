2689f80e9177b641e4acbff7e021384b2cb06a23
Fredrik Wollsén <fredrik@neam.se>
2016-06-07 13:45:06 +0300

    Removed a project-specific reference

--- stacks/neam/debian-php-nginx.dna-project-base/stack/start.sh
+++ stacks/neam/debian-php-nginx.dna-project-base/stack/start.sh
@@ -8,8 +8,8 @@ script_path=`dirname $0`
 cd $script_path/..
 
 # make sure no other stack's containers are using the ports that our stack uses
-docker ps | grep -v personalunit_ | grep '0.0.0.0:80->' | awk '{ print $1 }' | xargs docker kill
-docker ps | grep -v personalunit_ | grep '0.0.0.0:33306->' | awk '{ print $1 }' | xargs docker kill
+docker ps | grep -v _PROJECT_ | grep '0.0.0.0:80->' | awk '{ print $1 }' | xargs docker kill
+docker ps | grep -v _PROJECT_ | grep '0.0.0.0:33306->' | awk '{ print $1 }' | xargs docker kill
 
 # run actual command
 docker-stack local up --build -d
