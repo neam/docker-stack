6499492f70d8efcb352d2102cfa2651c15f9d51d
Fredrik Wollsén <fredrik@neam.se>
2016-06-07 13:44:43 +0300

    [Neam Debian PHP/Nginx] Working around permission errors locally by copying .ssh config temporarily instead of using host-volume-mounted ditos

--- stacks/neam/debian-php-nginx.dna-project-base/stack/src/install-deps.sh
+++ stacks/neam/debian-php-nginx.dna-project-base/stack/src/install-deps.sh
@@ -8,6 +8,11 @@ set -o errexit
 
 export PREFER=$PREFER
 
+if [ -d /root/.docker-host-ssh ]; then
+  echo '* Working around permission errors locally by copying .ssh config temporarily instead of using host-volume-mounted ditos'
+  cp -r /root/.docker-host-ssh /root/.ssh
+fi
+
 script_path=`dirname $0`
 $script_path/install-core-deps.sh
 $script_path/install-local-deps.sh
